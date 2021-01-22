f17941d89a15bcdf87693635d9e941a3d7574d6f
liyang <7325560@qq.com>
2021-01-22 16:32:48 +0800

    fix omnisharp-osx download url

--- omnisharp.rb
+++ omnisharp.rb
@@ -1,7 +1,7 @@
 class Omnisharp < Formula
   desc "Cross platform .NET development in the editor of your choice"
   homepage "http://www.omnisharp.net/"
-  url "https://github.com/OmniSharp/omnisharp-roslyn/releases/download/v1.37.6/omnisharp-mono.tar.gz"
+  url "https://github.com/OmniSharp/omnisharp-roslyn/releases/download/v1.37.6/omnisharp-osx.tar.gz"
   version "1.37.6"
   sha256 "a5e6c0536b35ae8fadabb02769528e203a138f1338759c02468d642680115be6"
   conflicts_with "omnisharp-mono"
