5ee7a82be6ea279ea82fb94f6966b7ba32e92442
liyang <7325560@qq.com>
2021-01-22 16:27:16 +0800

    upgrade omnisharp to 1.37.6

--- omnisharp-mono.rb
+++ omnisharp-mono.rb
@@ -1,9 +1,9 @@
 class OmnisharpMono < Formula
   desc "Cross platform .NET development in the editor of your choice"
   homepage "http://www.omnisharp.net/"
-  url "https://github.com/OmniSharp/omnisharp-roslyn/releases/download/v1.35.3/omnisharp-mono.tar.gz"
-  version "1.35.3"
-  sha256 "7e8a7b03abd697e1f961101c546eb9441c47a0406776661a659d1dddeb85c429"
+  url "https://github.com/OmniSharp/omnisharp-roslyn/releases/download/v1.37.6/omnisharp-mono.tar.gz"
+  version "1.37.6"
+  sha256 "e06d7a287a16d0c9290eb41085c7996e738c5cb0c2e59311c60c2455339295bc"
   conflicts_with "omnisharp"
 
   depends_on "mono" => :recommended
--- omnisharp.rb
+++ omnisharp.rb
@@ -1,9 +1,9 @@
 class Omnisharp < Formula
   desc "Cross platform .NET development in the editor of your choice"
   homepage "http://www.omnisharp.net/"
-  url "https://github.com/OmniSharp/omnisharp-roslyn/releases/download/v1.35.3/omnisharp-osx.tar.gz"
-  version "1.35.3"
-  sha256 "b498337e4cc5293744d1e0df999ad56d03191fc83ba72be2b71f529ebebe0177"
+  url "https://github.com/OmniSharp/omnisharp-roslyn/releases/download/v1.37.6/omnisharp-mono.tar.gz"
+  version "1.37.6"
+  sha256 "a5e6c0536b35ae8fadabb02769528e203a138f1338759c02468d642680115be6"
   conflicts_with "omnisharp-mono"
 
   depends_on "mono" => :recommended
